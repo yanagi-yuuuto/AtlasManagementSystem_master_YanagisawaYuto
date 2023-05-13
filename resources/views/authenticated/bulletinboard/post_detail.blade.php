63e46935d93750686cc6f229213bed583d39cc6c
yanagi-yuuto <yuto.jrm.uw@gmail.com>
2023-05-13 17:34:01 +0900

    投稿の編集/削除:修正(投稿者にのみ編集/削除ボタンを表示させる)

--- resources/views/authenticated/bulletinboard/post_detail.blade.php
+++ resources/views/authenticated/bulletinboard/post_detail.blade.php
@@ -8,8 +8,10 @@
           <div>
           </div>
           <div>
+            @if(Auth::user()->id == $post->user_id)
             <span class="edit-modal-open" post_title="{{ $post->post_title }}" post_body="{{ $post->post }}" post_id="{{ $post->id }}">編集</span>
             <a href="{{ route('post.delete', ['id' => $post->id]) }}" onclick="return confirm('削除してもよろしいですか？')">削除</a>
+            @endif
           </div>
         </div>
        @foreach($post->sub_categories as $sub_category)
