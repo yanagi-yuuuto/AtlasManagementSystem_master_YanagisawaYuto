8e68bebe3d3e205184ea8c651f4d7b238666ea48
yanagi-yuuto <yuto.jrm.uw@gmail.com>
2023-07-11 17:33:06 +0900

    スクール予約確認画面の各部数にリンクを設置する/ スクール予約確認画面の各部数のリンクから詳細ページへ遷移させる:完了

--- app/Calendars/Admin/CalendarWeekDay.php
+++ app/Calendars/Admin/CalendarWeekDay.php
@@ -31,13 +31,13 @@ class CalendarWeekDay{
 
     $html[] = '<div class="text-left">';
     if($one_part){
-      $html[] = '<p class="day_part m-0 pt-1">1部</p>';
+      $html[] = '<a href="route(\'authenticated.calendar.admin.reserve_detail\')"><p class="day_part m-0 pt-1">1部</p></a>';
     }
     if($two_part){
-      $html[] = '<p class="day_part m-0 pt-1">2部</p>';
+      $html[] = '<a href="route(\'authenticated.calendar.admin.reserve_detail\')"><p class="day_part m-0 pt-1">2部</p></a>';
     }
     if($three_part){
-      $html[] = '<p class="day_part m-0 pt-1">3部</p>';
+      $html[] = '<a href="route(\'authenticated.calendar.admin.reserve_detail\')"><p class="day_part m-0 pt-1">3部</p></a>';
     }
     $html[] = '</div>';
 
